da82e8fabe14a4711e02bba13f469ffa6685d5b8
Mulberry <wowcxy2008@126.com>
2018-01-10 09:36:44 +0800

    auto#ignore

---
+++ publish.js
@@ -0,0 +1,10 @@
+const qniu = require('qniu');
+
+const options = {
+  accessKey: 'KcH25vxJ8THYo9ExNp_-Kobr-TebdAZLss-JuLvD',
+  secretKey: 'y82r6nTrPeQpjs5UXRdHlwePZh_9nFKIS-6gJFEn',
+  bucket: 'static',
+  domain: 'ocgkyeaew.bkt.clouddn.com'
+};
+
+qniu(options);
